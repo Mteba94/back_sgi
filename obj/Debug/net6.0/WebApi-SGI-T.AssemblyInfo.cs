cdce56fbbd328ce5e3621b0105306f9047234f87
Melvin Tebalan <mtebaland@miumg.edu.gt>
2024-07-28 12:45:31 -0600

    Cambio nugget

--- obj/Debug/net6.0/WebApi-SGI-T.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApi-SGI-T.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi-SGI-T")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54bae9564dc82acdc16582d78332654c4c859352")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi-SGI-T")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi-SGI-T")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
